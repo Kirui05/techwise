a221f1d3cdcbee6f73d959deaa8a16efc5310dfb
Nicholas Kirui <nicholaskirui6@gmail.com>
2024-12-21 13:50:24 +0300

    corrections

--- wp-content/plugins/wpschoolpress/js/wpsp-custom-multicls.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-  $('.multiselect').selectpicker();
-  // $('select.multiselect').multiselect({
-  //   includeSelectAllOption: true,
-  //   nonSelectedText: 'Select Class'
-  // });
-});
-
